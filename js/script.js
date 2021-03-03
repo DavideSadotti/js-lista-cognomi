84b950dc611d80c327a31c9ab0eb2c546b7ef38d
Davide <d.sadotti@gmail.com>
2021-03-03 17:20:49 +0100

    Esercizio finito con controlli e richieste all'utente

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
 var cognomeUtente;
+var volere = true;
 
-cognomeUtente = prompt("Inserisci il tuo cognome");
-cognomeUtente = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.slice(1);
-cognomi.push(cognomeUtente);
+// RICHIESTA E CONTROLLO ALL'UTENTE SE VUOLE AGGIUNGERE UN NUOVO COGNOME
+do{
+  var scegli = prompt("Vuoi inserire un altro nome in lista? Rispondi con 's' se vuoi continuare ad aggiungere cognomi alla lista");
+  if(scegli.toLowerCase() == "s"){
+    cognomeUtente = prompt("Inserisci il tuo cognome");
+    cognomeUtente = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.slice(1);
+    cognomi.push(cognomeUtente);
+  }else {
+    volere = false;
+  }
+}while(volere == true)
 
 for(var i = 0; i < cognomi.length; i++){
   cognomi.sort();
-  document.getElementById("cognomi").innerHTML += "<li>" + cognomi[i] + "</li>";
+  document.getElementById("cognomi").innerHTML += "<li>Questo cognome si trova alla " + (i + 1) + "° posizione: " + cognomi[i] + "</li>";
 }
