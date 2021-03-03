e951dd23423ea641a2794ef50903984a4ade61dc
Davide <d.sadotti@gmail.com>
2021-03-03 16:57:40 +0100

    Inserisco un cognome nell'array e lo ordino

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+var cognomeUtente;
 
+cognomeUtente = prompt("Inserisci il tuo cognome");
+cognomeUtente = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.slice(1);
+cognomi.push(cognomeUtente);
+
+for(var i = 0; i < cognomi.length; i++){
+  cognomi.sort();
+  document.getElementById("cognomi").innerHTML += "<li>" + cognomi[i] + "</li>";
+}
